1f89e9320cc220ef4da967290282e1215cf97e2a
Shiba Kar <shibakar@swazei.com>
2022-09-17 10:34:32 +0530

    added home navigation screens

---
+++ lib/features/home/views/blood_donation_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BloodDonationView extends StatelessWidget {
+  const BloodDonationView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/features/home/views/views.dart
+++ lib/features/home/views/views.dart
@@ -1,1 +1,9 @@
 export 'home_view.dart';
+export 'book_ambulance_view.dart';
+export 'medicine_reminder.dart';
+export 'organ_donation_view.dart';
+export 'hospitals_nearby_view.dart';
+export 'my_doctor_view.dart';
+export 'blood_donation_view.dart';
+export 'my_medicine_history_view.dart';
+export 'my_medicine_history_view.dart';
