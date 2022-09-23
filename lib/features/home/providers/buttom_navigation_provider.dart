59933f8d61aa8d723d720ddfa19e66b5e9e4d6bb
Shiba Kar <shibakar@swazei.com>
2022-09-23 21:09:38 +0530

    screen name

--- lib/features/home/providers/buttom_navigation_provider.dart
+++ lib/features/home/providers/buttom_navigation_provider.dart
@@ -6,15 +6,38 @@ import '../pages/pages.dart';
 class ButtomNavigationProvider extends StateNotifier<Nav> {
   final Ref ref;
   final List<Map<String, dynamic>> pages = const [
-    {"widget": HomePage(), "label": "Home"},
-    {"widget": MembershipPage(), "label": "Membership"},
-    {"widget": WecarePage(), "label": "We care"},
-    {"widget": MorePage(), "label": "More"},
+    {"widget": HomePage(), "label": "Home", "pagelabel": "Services"},
+    {
+      "widget": MembershipPage(),
+      "label": "Membership",
+      "pagelabel": "My Membership"
+    },
+    {
+      "widget": WecarePage(),
+      "label": "We care",
+      "pagelabel": "Feedback",
+    },
+    {
+      "widget": MorePage(),
+      "label": "More",
+      "pagelabel": "More",
+    },
   ];
   ButtomNavigationProvider(this.ref)
-      : super(const Nav(index: 0, page: HomePage(), label: "Home"));
+      : super(
+          const Nav(
+            index: 0,
+            page: HomePage(),
+            label: "Home",
+            pagelabel: "Services",
+          ),
+        );
   onTap(int index) => state = Nav(
-      page: pages[index]['widget'], index: index, label: pages[index]['label']);
+        page: pages[index]['widget'],
+        index: index,
+        pagelabel: pages[index]["pagelabel"],
+        label: pages[index]['label'],
+      );
 }
 
 final buttomNavigationProvider =
